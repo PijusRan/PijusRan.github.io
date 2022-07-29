b88f69e666df484905d80c3a1f2eaa6234b0ac7f
pijusr <pijusranonis@gmail.com>
2022-07-29 09:58:32 +0300

    modified:   App.jsx 	deleted:    Icons/Facebook.svg 	deleted:    Icons/Git-2.37.1-64-bit.exe 	deleted:    Icons/Twitter.svg 	new file:   Icons/Youtube.png 	deleted:    Icons/albion-online-setup.exe 	modified:   components/Footer.jsx 	modified:   vite.config.js

--- App.jsx
+++ App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './App.css'
 import MainContent from "/components/MainContent.jsx"
 import Footer from "/components/Footer.jsx"
+import Photo from "/components/Photo.jpg"
 
 function App(){
   return(
     <container>
       <div className="Page">
-        <img src="./components/Photo.jpg" className="MyPhoto"/>
+        <img src={Photo} className="MyPhoto"/>
         <MainContent/>
         <Footer/>
       </div>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import Instagram from "../Icons/Instagram.svg"
+import Github from "../Icons/Github.svg"
+import Youtube from "../Icons/Youtube.png"
 
 export default function Footer(){
     return(
         <div className="Footer">
-            <a href="https://github.com/pijusr"><img src="../Icons/Github.svg"/></a>
-            <a href="https://www.instagram.com/frustedits/"><img src="../Icons/Instagram.svg"/></a>
-            <a href="https://www.facebook.com/PijusRanonis/"><img src="../Icons/Facebook.svg"/></a>
+            <a href="https://github.com/pijusr"><img src={Github}/></a>
+            <a href="https://www.instagram.com/frustedits/"><img src={Instagram}/></a>
+            <a href="https://www.youtube.com/c/Frusticks"><img src={Youtube}/></a>
         </div>
     )
 }
